931abbb46389d32b446be6d61b58a1a2e7d5b32e
Bshara Alhatem <bshara.h2003@gmail.com>
2026-06-04 20:03:52 +0300

    fix main

--- custom-addons/smart_hospital_pulse/controllers/main.py
+++ custom-addons/smart_hospital_pulse/controllers/main.py
@@ -13,7 +13,32 @@ class HospitalVitalsController(http.Controller):
     )
     def update_vitals(self, **kwargs):
 
-        return {
-            'status': 'ok',
-            'message': 'Controller Works'
-        }
+        try:
+            data = request.get_json_data()
+
+            ticket_id = data.get('ticket_id')
+            heart_rate = data.get('heart_rate')
+
+            ticket = request.env['crm.lead'].sudo().search(
+                [('id', '=', ticket_id)],
+                limit=1
+            )
+
+            if not ticket:
+                return {
+                    'status': 'error',
+                    'message': f'Ticket {ticket_id} not found'
+                }
+
+            return {
+                'status': 'ok',
+                'ticket_name': ticket.name,
+                'ticket_id': ticket.id,
+                'heart_rate': heart_rate
+            }
+
+        except Exception as e:
+            return {
+                'status': 'error',
+                'message': str(e)
+            }
